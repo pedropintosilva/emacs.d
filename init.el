e2cafef99d789b76dc9048d00aa9f6124ee207d4
Pedro Pinto Silva <pedro.silva@collabora.com>
2022-06-30 19:15:29 +0200

    Add sh-mode (shell-script-mode) and language server

--- init.el
+++ init.el
@@ -23,7 +23,7 @@
  '(custom-safe-themes
    '("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" default))
  '(package-selected-packages
-   '(dashboard minimap company-box magit css-ls rainbow-delimiters counsel-projectile projectile typescript-mode lsp-treemacs lsp-ui lsp-mode company spacemacs-theme which-key use-package)))
+   '(dashboard minimap company-box magit css-ls rainbow-delimiters counsel-projectile projectile typescript-mode shell-script-mode lsp-treemacs lsp-ui lsp-mode company spacemacs-theme which-key use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
