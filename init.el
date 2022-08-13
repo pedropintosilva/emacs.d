02d583fb93e011677970fcfa3162c47130f5ff38
Pedro Pinto da Silva <pedro.silva@collabora.com>
2022-08-13 16:22:14 +0200

    Use package: ensure always
    Up until now I though having every package I want to use with
    a dedicated ensure t would be better with the idea that it could
    allow me having listed packages no enabled.
    
    In practice I end up using all of them always so, better to
    ensure t always

--- init.el
+++ init.el
@@ -7,6 +7,8 @@
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
+(eval-and-compile
+  (setq use-package-always-ensure t))
 
 (unless (package-installed-p 'spacemacs-theme)
   (package-refresh-contents)
