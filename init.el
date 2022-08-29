145dd97556e2e00ba60e728547848c12136ae37d
Pedro Pinto Silva <pedro.silva@collabora.com>
2022-08-29 15:02:23 +0200

    Change themes

--- init.el
+++ init.el
@@ -10,10 +10,6 @@
 (eval-and-compile
   (setq use-package-always-ensure t))
 
-(unless (package-installed-p 'spacemacs-theme)
-  (package-refresh-contents)
-  (package-install 'spacemacs-theme))
-
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
 
 (custom-set-variables
@@ -21,9 +17,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(spacemacs-light))
- '(custom-safe-themes
-   '("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" default))
  '(package-selected-packages
    '(auto-revert-mode diminish rainbow restclient multiple-cursors-core multiple-cursors transpose-frame expand-region emacs-everywhere dashboard minimap company-box magit css-ls rainbow-delimiters counsel-projectile projectile typescript-mode shell-script-mode lsp-treemacs lsp-ui lsp-mode company spacemacs-theme which-key use-package)))
 (custom-set-faces
@@ -31,4 +24,3 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :extend nil :stipple nil :background "#fbf8ef" :foreground "#655370" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "ADBO" :family "Source Code Pro")))))
