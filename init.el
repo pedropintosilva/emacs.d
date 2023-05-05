1c6372849d12df0bf11eb7f179903f4c1ecf1fff
Pedro Pinto Silva <pedro.silva@collabora.com>
2023-05-05 09:29:19 +0200

    Disable unused spacemacs theme
    Signed-off-by: Pedro Pinto Silva <pedro.silva@collabora.com>

--- init.el
+++ init.el
@@ -8,7 +8,8 @@
   (package-refresh-contents)
   (package-install 'use-package))
 (eval-and-compile
-  (setq use-package-always-ensure t))
+  (setq use-package-always-ensure t
+        use-package-expand-minimally t))
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
 
@@ -18,7 +19,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(auto-revert-mode diminish rainbow restclient multiple-cursors-core multiple-cursors transpose-frame expand-region emacs-everywhere dashboard minimap company-box magit css-ls rainbow-delimiters counsel-projectile projectile typescript-mode shell-script-mode lsp-treemacs lsp-ui lsp-mode company spacemacs-theme which-key use-package)))
+   '(auto-revert-mode diminish rainbow restclient multiple-cursors-core multiple-cursors transpose-frame expand-region emacs-everywhere dashboard minimap company-box magit css-ls rainbow-delimiters counsel-projectile projectile typescript-mode shell-script-mode lsp-treemacs lsp-ui lsp-mode company which-key use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
