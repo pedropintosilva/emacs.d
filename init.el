2a6ccee8d3705e90804834c6cfab66e15c7b0a44
Pedro Pinto Silva <pedro.silva@collabora.com>
2023-10-25 09:33:13 +0200

    Fix missing parenthesis by removing empty duplicated custom-set-faces

--- init.el
+++ init.el
@@ -20,8 +20,3 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    '(auto-revert-mode diminish rainbow restclient multiple-cursors-core multiple-cursors transpose-frame expand-region emacs-everywhere dashboard minimap company-box magit css-ls rainbow-delimiters counsel-projectile projectile typescript-mode shell-script-mode lsp-treemacs lsp-ui lsp-mode company which-key use-package)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
